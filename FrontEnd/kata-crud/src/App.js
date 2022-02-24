8330605a5da85f63ccc5553a467b54d755751185
Jeaniel Osorno <jeanielosorno@gmail.com>
2022-02-24 18:57:15 -0500

    CRUD completo. Estado: funcionamiento igual a Guia parte 4

--- FrontEnd/kata-crud/src/App.js
+++ FrontEnd/kata-crud/src/App.js
@@ -3,7 +3,8 @@ import React, {createContext, useContext, useEffect, useReducer, useRef, useStat
 const HOST_API = "http://localhost:8080/api";
 
 const initialState = {
-  list: []
+  list: [],
+  item: {}
 };
 
 const Store = createContext(initialState);
@@ -12,8 +13,8 @@ const Store = createContext(initialState);
 const Form = () => {
 
   const formRef = useRef(null);
-  const {dispatch} = useContext(Store);
-  const [state, setState] = useState({});
+  const {dispatch, state:{item}} = useContext(Store);
+  const [state, setState] = useState(item);
 
   const onAdd = (event) => {
     event.preventDefault();
@@ -39,11 +40,36 @@ const Form = () => {
     });
   }
 
+  const onEdit = (event) => {
+    event.preventDefault();
+
+    const request = {
+      name: state.name,
+      id: item.id,
+      isCompleted: item.isCompleted
+    }
+
+    fetch(HOST_API+"/todo/update", {
+      method: "PUT",
+      body: JSON.stringify(request),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then((todo) => {
+      dispatch({type: "update-item", item: todo});
+      setState({name: ""});
+      formRef.current.reset();
+    });
+  }
+
   return <form ref={formRef}>
-    <input type="text" name="name" onChange={(event) => {
+    <input type="text" name="name" defaultValue={item.name} onChange={(event) => {
       setState({...state, name: event.target.value})
     }}></input>
-    <button onClick={onAdd}>Agregar</button>
+    {item.id && <button onClick={onEdit}>Editar</button>}
+    {!item.id && <button onClick={onAdd}>Agregar</button>}
   </form>
 }
 
@@ -60,6 +86,19 @@ const List = () => {
     })
   }, [state.list.length, dispatch]);
 
+  const onDelete = (id) => {
+    fetch(HOST_API+"/todo/delete/" + id, {
+      method: "DELETE"
+    })
+    .then((list) => {
+      dispatch({ type: "delete-item", id})
+    })
+  };
+
+  const onEdit = (todo) => {
+    dispatch({ type: "edit-item", item:todo})
+  };
+
   return (
     <div>
       <table>
@@ -75,7 +114,9 @@ const List = () => {
             return <tr key={todo.id}>
               <td>{todo.id}</td>
               <td>{todo.name}</td>
-              <td>{todo.isCompleted}</td>
+              <td>{todo.isCompleted === true ? "SI" : "NO"}</td>
+              <td><button onClick={() => onDelete(todo.id)}>Eliminar</button></td>
+              <td><button onClick={() => onEdit(todo)}>Editar</button></td>
             </tr>
           })}
         </tbody>
@@ -87,8 +128,23 @@ const List = () => {
 /* -------------------------------------------------------- */
 function reducer(state, action) {
   switch (action.type) {
+    case "update-item":
+      const listUpdateEdit = state.list.map((item) => {
+        if (item.id === action.item.id){
+          return action.item;
+        }
+        return item;
+      });
+      return { ...state, list: listUpdateEdit, item: {} }
+    case "delete-item":
+      const listUpdate = state.list.filter((item) => {
+        return item.id !== action.id;
+      });
+      return { ...state, list: listUpdate}
     case "update-list":
       return {...state, list: action.list}
+    case "edit-item":
+      return { ...state, item: action.item}
     case "add-item":
       const newList = state.list;
       newList.push(action.item);
