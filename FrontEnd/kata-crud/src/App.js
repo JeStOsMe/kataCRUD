d2ea0c7863d1d5ff9ccfea80087439e93263967d
Jeaniel Osorno <jeanielosorno@gmail.com>
2022-02-24 15:55:21 -0500

    Primeras funciones en FrontEnd. index.js modificado. App.js modificado: agregadas funciones flecha List y StoreProvider

--- FrontEnd/kata-crud/src/App.js
+++ FrontEnd/kata-crud/src/App.js
@@ -1,5 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {createContext, useContext, useReducer} from 'react';
+
+const HOST_API = 'http://localhost:8080/api';
+
+const initialState = {
+  list: []
+};
+
+const Store = createContext(initialState);
+
+const List = () => {
+  
+  const {dispatch, state} = useContext(Store);
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <td>ID</td>
+            <td>Nombre</td>
+            <td>¿Está completado?</td>
+          </tr>
+        </thead>
+        <tbody>
+          {state.list.map((todo) => {
+            return <tr key={todo.id}>
+              <td>{todo.id}</td>
+              <td>{todo.name}</td>
+              <td>{todo.isCompleted}</td>
+            </tr>
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+const StoreProvider = ( {children}) => {
+  function reducer(state, action) {
+    switch (action.type) {
+      case 'update-list':
+        return {...state, list: action.list}
+      case 'add-item':
+        const newList = state.list;
+        newList.push(action.item);
+        return {...state, list: newList}
+      default:
+        return state;
+    }
+  }
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return <Store.Provider value={{state, dispatch}}>
+    {children}
+  </Store.Provider>
+
+}
+
+
 
 function App() {
   return (
